89dc0039afad9186093816dba5fca4f81a81dc24
K Samarth N Kamath <kamathsamarth@gmail.com>
2023-02-15 12:53:48 +0530

    Create Split_to_Sentence.py

---
+++ Chapter1/Task1/Split_to_Sentence.py
@@ -0,0 +1,3 @@
+paragraph = "Hello there, I am Samarth. How are doing? I love coding in python and hope you will love it as well. Keep learning, we will rock it together!"
+sentence = paragraph.split(".")
+print(sentence)
