66c20aad242bca12bc56a6b48eea6d979b835cec
K Samarth N Kamath <kamathsamarth@gmail.com>
2023-02-15 12:56:43 +0530

    Create comma_seperated_sentence.py

---
+++ Chapter1/Task2/comma_seperated_sentence.py
@@ -0,0 +1,3 @@
+sentence = "Hello there, I am Samarth. How are doing? I love coding in python and hope you will love it as well. Keep learning, we will rock it together!"
+comma_seperated = sentence.split(",")
+print(comma_seperated)
