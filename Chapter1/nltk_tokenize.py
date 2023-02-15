2cc84a345077771cffe758b432e803df8374e6c3
K Samarth N Kamath <kamathsamarth@gmail.com>
2023-02-15 16:37:21 +0530

    Create nltk_tokenize.py

---
+++ Chapter1/nltk_tokenize.py
@@ -0,0 +1,5 @@
+from nltk.tokenize import word_tokenize
+
+s = '''Hello there, I am Samarth. How are doing? I love coding in python and hope you will love it as well. Keep learning, we will rock it together! Why don't you try something new with this. Good muffins cost $3.88\nin New York.  Please buy me two of them.\n\nThanks.'''
+
+print(word_tokenize(s)) 
